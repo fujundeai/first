32bbc216206770cc3b2962127834cccfd0e28328
fujun <1181234356@qq.com>
2021-06-30 17:26:43 +0800

    change

--- app/src/main/java/com/lqm/tomatoit/api/Cache.java
+++ app/src/main/java/com/lqm/tomatoit/api/Cache.java
@@ -1,4 +1,7 @@
 package com.lqm.tomatoit.api;
 
 public class Cache {
+    public void statt(){
+
+    }
 }
--- app/src/main/java/com/lqm/tomatoit/api/Test.java
+++ app/src/main/java/com/lqm/tomatoit/api/Test.java
@@ -3,5 +3,5 @@ package com.lqm.tomatoit.api;
 public class Test {
     private String name;
     private String age;
-
+    private int num;
 }
